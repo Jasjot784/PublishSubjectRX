c854f5764ce6c1c66cd45128e2e4817e45b3a5d5
Jasjot Singh <jasjot784@gmail.com>
2020-08-15 21:33:37 +0530

    Publish Subject

--- app/src/main/java/com/jasjotsingh/publishsubjectrx/MainActivity.java
+++ app/src/main/java/com/jasjotsingh/publishsubjectrx/MainActivity.java
@@ -3,12 +3,112 @@ package com.jasjotsingh.publishsubjectrx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.subjects.PublishSubject;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView textView;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doSomeWork();
+            }
+        });
     }
+
+    private void doSomeWork(){
+        PublishSubject<Integer> source = PublishSubject.create();
+
+        source.subscribe(getFirstObserver()); // it will get 1, 2, 3, 4 and onComplete
+
+        source.onNext(1);
+        source.onNext(2);
+        source.onNext(3);
+
+        /*
+         * it will emit 4 and onComplete for second observer also.
+         */
+        source.subscribe(getSecondObserver());
+
+        source.onNext(4);
+        source.onComplete();
+    }
+
+    private Observer<? super Integer> getFirstObserver() {
+        return new Observer<Integer>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, " First onSubscribe : " + d.isDisposed());
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                textView.append(" First onNext : value : " + integer);
+                textView.append("\n");
+                Log.d(TAG, " First onNext value : " + integer);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                textView.append(" First onError : " + e.getMessage());
+                textView.append("\n");
+                Log.d(TAG, " First onError : " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                textView.append(" First onComplete");
+                textView.append("\n");
+                Log.d(TAG, " First onComplete");
+            }
+        };
+    }
+    private Observer<? super Integer> getSecondObserver() {
+        return new Observer<Integer>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                textView.append(" Second onSubscribe : isDisposed :" + d.isDisposed());
+                Log.d(TAG, " Second onSubscribe : " + d.isDisposed());
+                textView.append("\n");
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                textView.append(" Second onNext : value : " + integer);
+                textView.append("\n");
+                Log.d(TAG, " Second onNext value : " + integer);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                textView.append(" Second onError : " + e.getMessage());
+                textView.append("\n");
+                Log.d(TAG, " Second onError : " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                textView.append(" Second onComplete");
+                textView.append("\n");
+                Log.d(TAG, " Second onComplete");
+            }
+        };
+    }
+
 }
